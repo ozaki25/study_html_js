fb453d3e554baad434637f9a63c52e7101c96246
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-04-27 07:46:40 +0900

    不要なファイルを削除

--- backbone/kadai1/scripts/collections/Users.js
+++
@@ -1,8 +0,0 @@
-var Backbone = require('backbone');
-var LocalStorage = require('backbone.LocalStorage');
-var User = require('../models/User');
-
-module.exports = Backbone.Collection.extend({
-    mdoel: User,
-    localStorage: new LocalStorage('backbone_sample'),
-});
