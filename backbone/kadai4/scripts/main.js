5a48c1d17341d05e4af479ed3c169847bae0693f
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-04-28 11:01:58 +0900

    Update main.js

--- backbone/kadai4/scripts/main.js
+++ backbone/kadai4/scripts/main.js
@@ -5,10 +5,10 @@ var Users = require('./collections/Users.js');
 var UsersView = require('./views/UsersView');
 
 var users = new Users([
-    {id: 1, name: '大谷', team: '日本ハム', position: 'ピッチャー'},
-    {id: 2, name: '藤浪', team: '阪神', position: 'ピッチャー'},
-    {id: 3, name: '中田', team: '日本ハム', position: 'ファースト'},
-    {id: 4, name: '筒香', team: '横浜DeNA', position: 'レフト'}
+    {name: '大谷', team: '日本ハム', position: 'ピッチャー'},
+    {name: '藤浪', team: '阪神', position: 'ピッチャー'},
+    {name: '中田', team: '日本ハム', position: 'ファースト'},
+    {name: '筒香', team: '横浜DeNA', position: 'レフト'}
 ]);
 var usersView = new UsersView({collection: users});
 usersView.render();
