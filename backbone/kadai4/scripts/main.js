2fc521feac1ae1581327de402767d9e2f44f3c2f
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-05-11 12:27:48 +0900

    Update main.js

--- backbone/kadai4/scripts/main.js
+++ backbone/kadai4/scripts/main.js
@@ -1,7 +1,7 @@
 var $ = jQuery = require('jquery');
 var _ = require('underscore');
 var Backbone = require('backbone');
-var Users = require('./collections/Users.js');
+var Users = require('./collections/Users');
 var UsersView = require('./views/UsersView');
 
 var users = new Users([
