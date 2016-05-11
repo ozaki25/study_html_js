1f604bea1acbb96aaf211c8f96f8bcedd42a59a1
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-05-11 12:25:35 +0900

    課題８縮小

--- backbone/kadai8/scripts/app.js
+++ backbone/kadai8/scripts/app.js
@@ -38,11 +38,7 @@ module.exports = Backbone.Model.extend({
     defaults: {
         name: '名無し',
         team: '無所属',
-        age: 0,
-        number: 999,
-        position: '全部',
-        career: 'なし',
-        title: 'なし'
+        position: '全部'
     }
 });
 
@@ -55,11 +51,7 @@ module.exports = Marionette.ItemView.extend({
     ui: {
         inputName: 'input#name',
         inputTeam: 'input#team',
-        inputAge: 'input#age',
-        inputNumber: 'input#number',
         inputPosition: 'input#position',
-        inputCareer: 'input#career',
-        inputTitle: 'input#title',
         inputs: 'input'
     },
     events: {
@@ -68,19 +60,11 @@ module.exports = Marionette.ItemView.extend({
     addUser: function() {
         var name = this.ui.inputName.val().trim();
         var team = this.ui.inputTeam.val().trim();
-        var age = this.ui.inputAge.val().trim();
-        var number = this.ui.inputNumber.val().trim();
         var position = this.ui.inputPosition.val().trim();
-        var career = this.ui.inputCareer.val().trim();
-        var title = this.ui.inputTitle.val().trim();
         var user = new User();
         if(name) user.set('name', name);
         if(team) user.set('team', team);
-        if(age) user.set('age', age);
-        if(number) user.set('number', number);
         if(position) user.set('position', position);
-        if(career) user.set('career', career);
-        if(title) user.set('title', title);
         this.collection.create(user);
         this.ui.inputs.val('');
     }
--- backbone/kadai8/scripts/models/User.js
+++ backbone/kadai8/scripts/models/User.js
@@ -4,10 +4,6 @@ module.exports = Backbone.Model.extend({
     defaults: {
         name: '名無し',
         team: '無所属',
-        age: 0,
-        number: 999,
-        position: '全部',
-        career: 'なし',
-        title: 'なし'
+        position: '全部'
     }
 });
