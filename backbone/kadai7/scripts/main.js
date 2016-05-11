cde32b4637f5b435157528185fd2a9da3bcc8e13
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-05-11 09:40:15 +0900

    headerを追加

--- backbone/kadai7/scripts/main.js
+++ backbone/kadai7/scripts/main.js
@@ -1,5 +1,6 @@
 var Marionette = require('backbone.marionette');
 var Users = require('./collections/Users.js');
+var HeaderView = require('./views/HeaderView');
 var UsersView = require('./views/UsersView');
 var FormView = require('./views/FormView');
 
@@ -7,11 +8,13 @@ var users = new Users();
 
 var app = new Marionette.Application({
     regions: {
+        header: '#header',
         users: '#users',
         newUser: '#new_user'
     },
     onStart: function() {
         users.fetch().done(function() {
+            this.header.show(new HeaderView());
             this.users.show(new UsersView({collection: users}));
             this.newUser.show(new FormView({collection: users}));
         }.bind(this));
